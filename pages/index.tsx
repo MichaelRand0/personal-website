ec37cff256bfca924261273eb6b60b1e047c63d0
MichaelRand0 <mihailstepanov.stepanoff2015@yandex.ru>
2023-08-28 00:26:44 +0300

    :sparkles: Features: Added cursor animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
-import { Inter } from "next/font/google"
 import Header from "$modules/header/Header"
+import dynamic from "next/dynamic"
 
-const inter = Inter({ subsets: ["latin"] })
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+})
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <AnimatedCursor
+        innerSize={26}
+        outerSize={26}
+        color="75, 255, 165"
+        outerAlpha={0.3}
+        innerScale={0}
+        innerStyle={{
+          display: 'none',
+          backgroundColor: '#4bffa599'
+        }}
+        outerScale={3}
+        showSystemCursor={true}
+        trailingSpeed={6}
+        clickables={[
+          "a",
+          "select",
+          "textarea",
+          "button",
+        ]}
+      />
     </main>
   )
 }
