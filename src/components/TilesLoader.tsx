861e52f31e03686d9340a9382db5fb8ca7b29857
MichaelRand0 <mihailstepanov.stepanoff2015@yandex.ru>
2023-09-05 13:06:39 +0300

    :art: Fixes: loading animation fixed

--- src/components/TilesLoader.tsx
+++ src/components/TilesLoader.tsx
@@ -4,7 +4,7 @@ interface Props extends React.ComponentProps<'div'> {}
 const TilesLoader = (props: Props) => {
   const {} = props
   return (
-    <div className="absolute w-full h-full left-0 top-0 z-[100] fill-mode-forwards animate-hide delay-2800 overflow-hidden">
+    <div className="absolute w-full h-full left-0 top-0 z-[100] fill-mode-forwards animate-hide delay-3500 overflow-hidden">
       <div className="animate-showDown absolute top-0 left-[50%] fill-mode-forwards duration-1500 bg-[white] w-[2px] z-[70] right-[50%] transition -translate-x-[50%]"></div>
       <div className="absolute top-0 flex left-0 w-full h-full z-[60]">
         <div className="w-1/5 h-full bg-greyStrong animate-scrollTop delay-2s fill-mode-forwards duration-500"></div>
